fbc1f1a17be500ef7713f691c7e0bbe6b5f514d2
Richard Liebscher <r1tschy@posteo.de>
2022-01-30 21:36:13 +0100

    Ignore constants for cbindgen

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
+/// cbindgen:ignore
 pub const ORGANIZATION_NAME: &str = "de.richardliebscher";
+/// cbindgen:ignore
 pub const APPLICATION_NAME: &str = "sailify";
+/// cbindgen:ignore
 pub const PACKAGE_NAME: &str = "harbour-sailify";
 
 pub mod player;
